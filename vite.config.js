3baf3ca6711bdb6cb6acd14d314064263cdb0537
saniakulsum <saniakulsum18@gmail.com>
2025-11-19 14:39:10 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,5 @@ import { defineConfig } from 'vite';
 import fs from 'fs';
 
 export default defineConfig({
-  server: {
-    https: {
-      key: fs.readFileSync('./Keys/kronos88-privateKey.key'),
-      cert: fs.readFileSync('./Keys/kronos88.crt'),
-    },
-  },
+  
 });
